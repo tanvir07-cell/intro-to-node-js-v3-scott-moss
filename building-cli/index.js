f5ae70fc2a105a77738585ad21ed42698958f03f
unknown <tr9836859@gmail.com>
2023-06-15 14:17:43 +0600

    chore:for the building_cli modules each logic

--- building-cli/index.js
+++ building-cli/index.js
@@ -1,11 +1,3 @@
 #!/usr/bin/env node
 
-// taking input using console:
-const notes = process.argv[2];
-
-const createNote = {
-  content: notes,
-  id: Date.now(),
-};
-
-console.log(createNote);
+import "./src/command.js";
